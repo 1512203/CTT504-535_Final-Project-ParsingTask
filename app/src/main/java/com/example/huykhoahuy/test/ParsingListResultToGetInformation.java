f74c33bc4e0f96a2459efd40dbbbe55258a50d04
Quoc Huy Nguyen <lawrencealgorithm@gmail.com>
2018-06-30 16:41:10 +0700

    Edit ParsingListResultToGetInformation.java

--- app/src/main/java/com/example/huykhoahuy/test/ParsingListResultToGetInformation.java
+++ app/src/main/java/com/example/huykhoahuy/test/ParsingListResultToGetInformation.java
@@ -3,14 +3,18 @@ package com.example.huykhoahuy.test;
 import java.util.ArrayList;
 
 class ParsingListResultToGetInformation {
-
-
     public LotteryInfo getLotteryInfo(ArrayList<String> listResult) {
         for (int i = 0; i < listResult.size(); ++i) {
             // Says, we only care about the continuous recognized results
             // For example: 6 4 2 3 9 7 (the space is optional)
             // For example: SO XO HAU GIANG
             // etc.
+
+            String[] resultInOneBlock = listResult.get(i).split("\n");
+            for (int j = 0; j < resultInOneBlock.length; ++j) {
+                String resultInABlock = resultInOneBlock[j];
+                
+            }
         }
         return null;
     }
